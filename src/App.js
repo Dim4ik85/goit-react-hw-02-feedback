70f035ffae67fde2a1f897745b9ce8f1d5d06f79
Dmytro Sagaidachnyi <lafuma4me@gmail.com>
2021-06-22 00:24:56 +0300

    App, components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Layout from './components/Layout';
+import Section from './components/Section';
+import Statistics from './components/Statistics';
+import FeedbackOptions from './components/FeedbackOptions';
+import Notification from './components/Notification';
+
+
+class App extends React.Component { 
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+
+  countTotalFeedback() {
+
+  }
+  
+  countPositiveFeedbackPercentage() {
+
+  }
+
+
+  render () {
+
+    return (
+      <div className="Layout">
+        <Layout>
+            <Section title="Please leave feedback">
+              <FeedbackOptions 
+                options={} 
+                onLeaveFeedback={} />
+            </Section>
+
+            <Section title="Statistics">  
+              <Statistics
+                good={}
+                neutral={}
+                bad={}
+                total={}
+                positivePercentage={} />
+            
+                : <Notification message="No feedback given" />
+            </Section>
+        </Layout>
+      </div>
+    );
+  }
 }
+
 
 export default App;
